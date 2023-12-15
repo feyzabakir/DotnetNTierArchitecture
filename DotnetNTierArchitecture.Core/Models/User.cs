8cdd61f30c5f8f3e486f2211221093f34c93ce29
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2023-12-15 21:42:44 +0300

    Entities and Repositories were created in the Core Layer

---
+++ DotnetNTierArchitecture.Core/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetNTierArchitecture.Core.Models
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+        public int TeamId { get; set; }//Foreign Key
+        public Team Team { get; set; } // One to Many navigation property
+    }
+}
---
+++ DotnetNTierArchitecture.Core/Models/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetNTierArchitecture.Core.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public User User { get; set; } // One to one
+        public Team Team { get; set; }
+    }
+}
